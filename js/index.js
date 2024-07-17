7354d2776e9b6aef2f2ee7d635c1572069d21bcf
chouchou77 <khalfawi331@gmail.com>
2024-07-17 18:40:00 +0100

    front end

---
+++ js/index.js
@@ -0,0 +1,259 @@
+// ENGLISH
+function Navbar() {
+  return /*#__PURE__*/React.createElement("header", null, /*#__PURE__*/React.createElement("nav", {
+    className: "navbar navbar-expand-lg"
+  }, /*#__PURE__*/React.createElement("a", {
+    href: "index.html"
+  }, /*#__PURE__*/React.createElement("img", {
+    alt: "logo of chouchou 77",
+    src: "images/logo.png",
+    className: "navbar-brand"
+  })), /*#__PURE__*/React.createElement("a", {
+    href: "#show",
+    "data-bs-toggle": "collapse",
+    className: "navbar-toggler"
+  }, /*#__PURE__*/React.createElement("span", {
+    className: "navbar-toggler-icon"
+  })), /*#__PURE__*/React.createElement("div", {
+    id: "show",
+    className: "collapse navbar-collapse"
+  }, /*#__PURE__*/React.createElement("ul", {
+    className: "navbar-nav"
+  }, /*#__PURE__*/React.createElement("li", {
+    className: "nav-item"
+  }, /*#__PURE__*/React.createElement("a", {
+    href: "#home",
+    className: "nav-link"
+  }, "HOME")), /*#__PURE__*/React.createElement("li", {
+    className: "nav-item"
+  }, /*#__PURE__*/React.createElement("a", {
+    href: "#about_me",
+    className: "nav-link"
+  }, "ABOUT ME")), /*#__PURE__*/React.createElement("li", {
+    className: "nav-item"
+  }, /*#__PURE__*/React.createElement("a", {
+    href: "#contact",
+    className: "nav-link"
+  }, "CONTACT")), /*#__PURE__*/React.createElement("li", {
+    className: "nav-item"
+  }, /*#__PURE__*/React.createElement("a", {
+    href: "#skills",
+    className: "nav-link"
+  }, "SKILLS")), /*#__PURE__*/React.createElement("li", {
+    className: "nav-item"
+  }, /*#__PURE__*/React.createElement("a", {
+    href: "#projects",
+    className: "nav-link"
+  }, "PROJECTS"))))));
+}
+function Home() {
+  return /*#__PURE__*/React.createElement("section", {
+    id: "home"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "myImg"
+  }, /*#__PURE__*/React.createElement("img", {
+    src: "images/myImg.png",
+    alt: "image of chouchou77"
+  })), /*#__PURE__*/React.createElement("div", {
+    className: "me"
+  }, /*#__PURE__*/React.createElement("h1", null, /*#__PURE__*/React.createElement("span", {
+    className: "mn"
+  }, "I'm  "), /*#__PURE__*/React.createElement("span", {
+    className: "nm"
+  }, "CHIHAB EDDIN KHALFAOUI")), /*#__PURE__*/React.createElement("p", null, "front end developer"), /*#__PURE__*/React.createElement("a", {
+    href: "#contact"
+  }, /*#__PURE__*/React.createElement("button", {
+    className: "btn"
+  }, "CONTACT ME NOW"))));
+}
+function AboutMe() {
+  return /*#__PURE__*/React.createElement("section", {
+    id: "about_me"
+  }, /*#__PURE__*/React.createElement("h1", null, "ABOUT ME"), /*#__PURE__*/React.createElement("p", null, "my name is ", /*#__PURE__*/React.createElement("span", {
+    className: "mar"
+  }, "chihab eddin khalfaoui"), ", im 18 from city of constantine, Algeria. I'm ", /*#__PURE__*/React.createElement("span", {
+    className: "mar"
+  }, "front end developer"), ", i have 3 years of experience in bulding projects web and front end ,and i have ", /*#__PURE__*/React.createElement("span", {
+    className: "mar"
+  }, "many skills"), " ,and I have the potential to ", /*#__PURE__*/React.createElement("span", {
+    className: "mar"
+  }, "learn quickly")," I am fluent in " ,/*#__PURE__*/React.createElement("span", {
+    className: "mar"
+  }, "Arabic and English and French"),", I have some certificates from" ,/*#__PURE__*/React.createElement("span", {
+    className: "mar"
+  }, "Free Code Camp"),"this is ",/*#__PURE__*/React.createElement("a", {
+    className: "mar",
+    href: "https://freecodecamp.org/certification/Chouchou77/responsive-web-design"
+  }, "certification in html&css")," and this ",/*#__PURE__*/React.createElement("a", {
+    className: "mar",
+    href: "https://freecodecamp.org/certification/Chouchou77/javascript-algorithms-and-data-structures"
+  }, "certification in js"),",and this ",/*#__PURE__*/React.createElement("a", {
+    className: "mar",
+    href: "https://freecodecamp.org/certification/Chouchou77/front-end-development-libraries"
+  }, "certification in library and frameWork"),", finally this",React.createElement("a", {
+    className: "mar",
+    href: "https://freecodecamp.org/certification/Chouchou77/data-visualization"
+  }, "certification in d3 frameWork and use API's")," .I am available for any job, full-time, part-time or freelance "));
+}
+function Contact() {
+  return /*#__PURE__*/React.createElement("section", {
+    id: "contact"
+  }, /*#__PURE__*/React.createElement("h1", null, "CONTACT"), /*#__PURE__*/React.createElement("a", {
+    href: "mailto:khalfawi331@gmail.com"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "con email"
+  }, /*#__PURE__*/React.createElement("i", {
+    className: "fa-solid fa-at"
+  }), /*#__PURE__*/React.createElement("p", null, "khalfawi331@gmail.com"))), /*#__PURE__*/React.createElement("a", {
+    href: "wa.me/213794433372?text="
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "con whatsapp"
+  }, /*#__PURE__*/React.createElement("i", {
+    className: "fa-brands fa-whatsapp"
+  }), /*#__PURE__*/React.createElement("p", null, "+213794433372"))), /*#__PURE__*/React.createElement("a", {
+    href: "2u.pw/k8tE0HPU"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "con viber"
+  }, /*#__PURE__*/React.createElement("i", {
+    className: "fa-brands fa-viber"
+  }), /*#__PURE__*/React.createElement("p", null, "+213794433372"))), /*#__PURE__*/React.createElement("a", {
+    href: "t.me/+213794433372"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "con telegram"
+  }, /*#__PURE__*/React.createElement("i", {
+    className: "fa-brands fa-telegram"
+  }), /*#__PURE__*/React.createElement("p", null, "+213794433372"))));
+}
+function Skills() {
+  return /*#__PURE__*/React.createElement("section", {
+    id: "skills"
+  }, /*#__PURE__*/React.createElement("h1", null, "SKILLS"), /*#__PURE__*/React.createElement("ul", {
+    class: "skills"
+  }, /*#__PURE__*/React.createElement("li", null, "HTML"), /*#__PURE__*/React.createElement("li", null, "CSS"), /*#__PURE__*/React.createElement("li", null, "JS(ES6)"), /*#__PURE__*/React.createElement("li", null, "AJAX&JSON"), /*#__PURE__*/React.createElement("li", null, "API'S"), /*#__PURE__*/React.createElement("li", null, "GIT&GITHUB"), /*#__PURE__*/React.createElement("li", null, "BOTSTRAP"), /*#__PURE__*/React.createElement("li", null, "jQUERY"), /*#__PURE__*/React.createElement("li", null, "SCSS"), /*#__PURE__*/React.createElement("li", null, "TYPESCRIPT"), /*#__PURE__*/React.createElement("li", null, "ReqctJS"),React.createElement("li", null, "D3"), /*#__PURE__*/React.createElement("li", null, "REDUX")));
+}
+function Projects() {
+  return /*#__PURE__*/React.createElement("section", {
+    id: "projects"
+  }, /*#__PURE__*/React.createElement("h1", null, "MY PROJECTS"), /*#__PURE__*/React.createElement("a", {
+    href: "http://chouchou.free.nf/WeatherApp/"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "card"
+  }, /*#__PURE__*/React.createElement("img", {
+    src: "https://chouchou77.000webhostapp.com/PORTFOLIO/images/1.png",
+    className: "card-img"
+  }), /*#__PURE__*/React.createElement("div", {
+    className: "card-body"
+  }, /*#__PURE__*/React.createElement("p", null, "Weather App")))), /*#__PURE__*/React.createElement("a", {
+    href: "http://chouchou.free.nf/ToDoList/"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "card"
+  }, /*#__PURE__*/React.createElement("img", {
+    src: "../images/2.png",
+    className: "card-img"
+  }), /*#__PURE__*/React.createElement("div", {
+    className: "card-body"
+  }, /*#__PURE__*/React.createElement("p", null, "TO DO list")))), /*#__PURE__*/React.createElement("a", {
+    href: "http://chouchou.free.nf/WHOWINMILLION/"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "card"
+  }, /*#__PURE__*/React.createElement("img", {
+    src: "../images/3.png",
+    className: "card-img"
+  }), /*#__PURE__*/React.createElement("div", {
+    className: "card-body"
+  }, /*#__PURE__*/React.createElement("p", null, "who win MILLION")))), /*#__PURE__*/React.createElement("a", {
+    href: "http://chouchou.free.nf/PORTFOLIO"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "card"
+  }, /*#__PURE__*/React.createElement("img", {
+    src: "../images/4.png",
+    className: "card-img"
+  }), /*#__PURE__*/React.createElement("div", {
+    className: "card-body"
+  }, /*#__PURE__*/React.createElement("p", null, "PORTFOLIO")))), /*#__PURE__*/React.createElement("a", {
+    href: "http://chouchou.free.nf/PsdToHtml1/"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "card"
+  }, /*#__PURE__*/React.createElement("img", {
+    src: "../images/5.png",
+    className: "card-img"
+  }), /*#__PURE__*/React.createElement("div", {
+    className: "card-body"
+  }, /*#__PURE__*/React.createElement("p", null, "PSD to HTML template")))), /*#__PURE__*/React.createElement("a", {
+    href: "http://chouchou.free.nf/PsdToHtml0/"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "card"
+  }, /*#__PURE__*/React.createElement("img", {
+    src: "../images/6.png",
+    className: "card-img"
+  }), /*#__PURE__*/React.createElement("div", {
+    className: "card-body"
+  }, /*#__PURE__*/React.createElement("p", null, "PSD to HTML template")))));
+}
+function Footer(){
+  return /*#__PURE__*/React.createElement("footer", null, /*#__PURE__*/React.createElement("button", {
+    className: "modeD",
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "noActive tog"
+  })),React.createElement("span",null,"DARK MODE"), /*#__PURE__*/React.createElement("select", null, /*#__PURE__*/React.createElement("option", {
+    value: "en"
+  }, "En \uD83C\uDDEC\uD83C\uDDE7"), /*#__PURE__*/React.createElement("option", {
+    value: "ar"
+  }, "Ar \uD83C\uDDF8\uD83C\uDDE6")), /*#__PURE__*/React.createElement("span", null, "Copyright 2024 Hanvesting All Rights Reserved"));
+}
+class MyApp extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(Navbar, null), /*#__PURE__*/React.createElement(Home, null), /*#__PURE__*/React.createElement(AboutMe, null), /*#__PURE__*/React.createElement(Contact, null), /*#__PURE__*/React.createElement(Skills, null), /*#__PURE__*/React.createElement(Projects, null), /*#__PURE__*/React.createElement(Footer, null));
+  }
+}
+ReactDOM.render( /*#__PURE__*/React.createElement(MyApp, null), document.querySelector("#root"));
+var select = document.querySelector("select");
+select.onchange = () => {
+  if(select.value == "ar"){
+    select.value = "en"
+    window.open("indexA.html")
+  }
+}
+//dark mode
+var varia = document.querySelector(":root");
+var darkM = document.querySelector(".modeD");
+var toggle = document.querySelector(".tog");
+var about = document.querySelector("#about_me");
+var contact = document.querySelector("#contact");
+var skills = document.querySelector("#skills");
+var projects = document.querySelector("#projects");
+var nav = document.querySelector(".navbar");
+var show = document.querySelector("#show");
+var foot = document.querySelector("footer");
+function b() {
+  if(toggle.classList[0] === "active" || toggle.classList[1] === "active"){
+    toggle.classList.replace("active","noActive");
+    about.style.backgroundImage = 'url("../images/AboutMeL.png")';
+    contact.style.backgroundImage = 'url("../images/conL.png")';
+    skills.style.backgroundImage = 'url("../images/AboutMeL.png")';
+    projects.style.backgroundImage = 'url("../images/conL.png")';
+    nav.style.background = "rgba(216, 217, 218,.7)";
+    show.style.background = "rgba(216, 217, 218,.7)";
+    varia.style.setProperty("--black","#FFF");
+    varia.style.setProperty("--white","#000");
+    varia.style.setProperty("--dark","#e8e9ea");
+    varia.style.setProperty("--light","#212529");
+  }
+  else if(toggle.classList[0] === "noActive" || toggle.classList[1] === "noActive"){
+    toggle.classList.replace("noActive","active");
+    about.style.backgroundImage = 'url("../images/AboutMeD.png")'
+    contact.style.backgroundImage = 'url("../images/conD.png")'
+    skills.style.backgroundImage = 'url("../images/AboutMeD.png")'
+    projects.style.backgroundImage = 'url("../images/conD.png")'
+    nav.style.background = "rgba(21, 25, 29,.7)";
+    show.style.background = "rgba(21, 25, 29,.7)";
+    varia.style.setProperty("--black","#000");
+    varia.style.setProperty("--white","#FFF");
+    varia.style.setProperty("--dark","#212529");
+    varia.style.setProperty("--light","#e8e9ea");
+  }
+}
+darkM.onclick = b
---
+++ js/indexA.js
@@ -0,0 +1,265 @@
+// ARABIC
+function NavbarA() {
+      return /*#__PURE__*/React.createElement("header", null, /*#__PURE__*/React.createElement("nav", {
+        className: "navbar navbar-expand-lg"
+      }, /*#__PURE__*/React.createElement("a", {
+        href: "index.html"
+      }, /*#__PURE__*/React.createElement("img", {
+        alt: "logo of chouchou 77",
+        src: "images/logo.png",
+        className: "navbar-brand"
+      })), /*#__PURE__*/React.createElement("a", {
+        href: "#show",
+        "data-bs-toggle": "collapse",
+        className: "navbar-toggler"
+      }, /*#__PURE__*/React.createElement("span", {
+        className: "navbar-toggler-icon"
+      })), /*#__PURE__*/React.createElement("div", {
+        id: "show",
+        className: "collapse navbar-collapse"
+      }, /*#__PURE__*/React.createElement("ul", {
+        className: "navbar-nav"
+      }, /*#__PURE__*/React.createElement("li", {
+        className: "nav-item"
+      }, /*#__PURE__*/React.createElement("a", {
+        href: "#home",
+        className: "nav-link"
+      }, "الرئيسية")), /*#__PURE__*/React.createElement("li", {
+        className: "nav-item"
+      }, /*#__PURE__*/React.createElement("a", {
+        href: "#about_me",
+        className: "nav-link"
+      }, "عني")), /*#__PURE__*/React.createElement("li", {
+        className: "nav-item"
+      }, /*#__PURE__*/React.createElement("a", {
+        href: "#contact",
+        className: "nav-link"
+      }, "التواصل معي")), /*#__PURE__*/React.createElement("li", {
+        className: "nav-item"
+      }, /*#__PURE__*/React.createElement("a", {
+        href: "#skills",
+        className: "nav-link"
+      }, "مهارات")), /*#__PURE__*/React.createElement("li", {
+        className: "nav-item"
+      }, /*#__PURE__*/React.createElement("a", {
+        href: "#projects",
+        className: "nav-link"
+      }, "مشاريعي"))))));
+    }
+function HomeA() {
+  return /*#__PURE__*/React.createElement("section", {
+    id: "home"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "myImg"
+  }, /*#__PURE__*/React.createElement("img", {
+    src: "images/myImg.png",
+    alt: "image of chouchou77"
+  })), /*#__PURE__*/React.createElement("div", {
+    className: "me"
+  }, /*#__PURE__*/React.createElement("h1", null, /*#__PURE__*/React.createElement("span", {
+    className: "mn"
+  }, "أنا "), /*#__PURE__*/React.createElement("span", {
+    className: "nm"
+  }, "شهاب الدين خلفاوي")), /*#__PURE__*/React.createElement("p", null, "front end developer"), /*#__PURE__*/React.createElement("a", {
+    href: "#contact"
+  }, /*#__PURE__*/React.createElement("button", {
+    className: "btn"
+  }, "تواصل معي الآن"))));
+}
+function AboutMeA() {
+  return /*#__PURE__*/React.createElement("section", {
+    id: "about_me"
+  }, /*#__PURE__*/React.createElement("h1", null, "عني"), /*#__PURE__*/React.createElement("p", null, "اسمي ", /*#__PURE__*/React.createElement("span", {
+    className: "mar"
+  }, "شهاب الدين خلفاوي"), "، عمري 18 عامًا من مدينة قسنطينة، الجزائر. أنا ", /*#__PURE__*/React.createElement("span", {
+    className: "mar"
+  }, "مطور واجهات أمامية"), "، لدي 3 سنوات من الخبرة في بناء المشاريع الويب والواجهات الأمامية، ولدي ", /*#__PURE__*/React.createElement("span", {
+    className: "mar"
+  }, "العديد من المهارات"), "، ولدي القدرة على ", /*#__PURE__*/React.createElement("span", {
+    className: "mar"
+  }, "التعلم بسرعة"), " أنا أتقن ", /*#__PURE__*/React.createElement("span", {
+    className: "mar"
+  }, "العربية والإنجليزية والفرنسية"), " لدي بعض الشهادات من", /*#__PURE__*/React.createElement("span", {
+    className: "mar"
+  }, "Free Code Camp"), " هذه هي ", /*#__PURE__*/React.createElement("a", {
+    className: "mar",
+    href: "https://freecodecamp.org/certification/Chouchou77/responsive-web-design"
+  }, "شهادة في بناء صفحات ويب"), " وهذه ", /*#__PURE__*/React.createElement("a", {
+    className: "mar",
+    href: "https://freecodecamp.org/certification/Chouchou77/javascript-algorithms-and-data-structures"
+  }, "شهادة في الجافاسكريبت"), " و هذه ", /*#__PURE__*/React.createElement("a", {
+    className: "mar",
+    href: "https://freecodecamp.org/certification/Chouchou77/front-end-development-libraries"
+  }, "شهادة في المكتبات و الأطر")," و أخيرا هذه " ,React.createElement("a", {
+    className: "mar",
+    href: "https://freecodecamp.org/certification/Chouchou77/data-visualization"
+  }, "شهادة في الدي3 و واجهة برمجة التطبيقات"),
+  ".أنا متاح لأي وظيفة، بدوام كامل، بدوام جزئي أو عمل حر "));
+}
+function ContactA() {
+  return /*#__PURE__*/React.createElement("section", {
+    id: "contact"
+  }, /*#__PURE__*/React.createElement("h1", null, "التواصل معي"), /*#__PURE__*/React.createElement("a", {
+    href: "mailto:khalfawi331@gmail.com"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "con email"
+  }, /*#__PURE__*/React.createElement("i", {
+    className: "fa-solid fa-at"
+  }), /*#__PURE__*/React.createElement("p", null, "khalfawi331@gmail.com"))), /*#__PURE__*/React.createElement("a", {
+    href: "wa.me/213794433372?text="
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "con whatsapp"
+  }, /*#__PURE__*/React.createElement("i", {
+    className: "fa-brands fa-whatsapp"
+  }), /*#__PURE__*/React.createElement("p", null, "+213794433372"))), /*#__PURE__*/React.createElement("a", {
+    href: "2u.pw/k8tE0HPU"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "con viber"
+  }, /*#__PURE__*/React.createElement("i", {
+    className: "fa-brands fa-viber"
+  }), /*#__PURE__*/React.createElement("p", null, "+213794433372"))), /*#__PURE__*/React.createElement("a", {
+    href: "t.me/+213794433372"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "con telegram"
+  }, /*#__PURE__*/React.createElement("i", {
+    className: "fa-brands fa-telegram"
+  }), /*#__PURE__*/React.createElement("p", null, "+213794433372"))));
+}
+function SkillsA() {
+  return /*#__PURE__*/React.createElement("section", {
+    id: "skills"
+  }, /*#__PURE__*/React.createElement("h1", null, "مهارات"), /*#__PURE__*/React.createElement("ul", {
+    class: "skills"
+  }, /*#__PURE__*/React.createElement("li", null, "HTML"), /*#__PURE__*/React.createElement("li", null, "CSS"), /*#__PURE__*/React.createElement("li", null, "JS(ES6)"), /*#__PURE__*/React.createElement("li", null, "AJAX&JSON"), /*#__PURE__*/React.createElement("li", null, "API'S"), /*#__PURE__*/React.createElement("li", null, "GIT&GITHUB"), /*#__PURE__*/React.createElement("li", null, "BOTSTRAP"), /*#__PURE__*/React.createElement("li", null, "jQUERY"), /*#__PURE__*/React.createElement("li", null, "SCSS"), /*#__PURE__*/React.createElement("li", null, "TYPESCRIPT"), /*#__PURE__*/React.createElement("li", null, "ReqctJS"), React.createElement("li", null, "D3"),/*#__PURE__*/React.createElement("li", null, "REDUX")));
+}
+function ProjectsA() {
+  return /*#__PURE__*/React.createElement("section", {
+    id: "projects"
+  }, /*#__PURE__*/React.createElement("h1", null, "مشاريعي"), /*#__PURE__*/React.createElement("a", {
+    href: "http://chouchou.free.nf/WeatherApp/"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "card"
+  }, /*#__PURE__*/React.createElement("img", {
+    src: "../images/1.png",
+    className: "card-img"
+  }), /*#__PURE__*/React.createElement("div", {
+    className: "card-body"
+  }, /*#__PURE__*/React.createElement("p", null, "Weather App")))), /*#__PURE__*/React.createElement("a", {
+    href: "http://chouchou.free.nf/ToDoList/"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "card"
+  }, /*#__PURE__*/React.createElement("img", {
+    src: "../images/2.png",
+    className: "card-img"
+  }), /*#__PURE__*/React.createElement("div", {
+    className: "card-body"
+  }, /*#__PURE__*/React.createElement("p", null, "TO DO list")))), /*#__PURE__*/React.createElement("a", {
+    href: "http://chouchou.free.nf/WHOWINMILLION/"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "card"
+  }, /*#__PURE__*/React.createElement("img", {
+    src: "../images/3.png",
+    className: "card-img"
+  }), /*#__PURE__*/React.createElement("div", {
+    className: "card-body"
+  }, /*#__PURE__*/React.createElement("p", null, "who win MILLION")))), /*#__PURE__*/React.createElement("a", {
+    href: "http://chouchou.free.nf/PORTFOLIO"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "card"
+  }, /*#__PURE__*/React.createElement("img", {
+    src: "../images/4.png",
+    className: "card-img"
+  }), /*#__PURE__*/React.createElement("div", {
+    className: "card-body"
+  }, /*#__PURE__*/React.createElement("p", null, "PORTFOLIO")))), /*#__PURE__*/React.createElement("a", {
+    href: "http://chouchou.free.nf/PsdToHtml1/"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "card"
+  }, /*#__PURE__*/React.createElement("img", {
+    src: "../images/5.png",
+    className: "card-img"
+  }), /*#__PURE__*/React.createElement("div", {
+    className: "card-body"
+  }, /*#__PURE__*/React.createElement("p", null, "PSD to HTML template")))), /*#__PURE__*/React.createElement("a", {
+    href: "http://chouchou.free.nf/PsdToHtml0/"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "card"
+  }, /*#__PURE__*/React.createElement("img", {
+    src: "../images/6.png",
+    className: "card-img"
+  }), /*#__PURE__*/React.createElement("div", {
+    className: "card-body"
+  }, /*#__PURE__*/React.createElement("p", null, "PSD to HTML template")))));
+}
+function FooterA() {
+  return /*#__PURE__*/React.createElement("footer", null, /*#__PURE__*/React.createElement("div", {
+    className: "modeD"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "noActive tog"
+  })),React.createElement("span",null,"الوضع المظلم"), /*#__PURE__*/React.createElement("select", null, /*#__PURE__*/React.createElement("option", {
+    value: "ar"
+  }, "Ar \uD83C\uDDF8\uD83C\uDDE6"), /*#__PURE__*/React.createElement("option", {
+    value: "en"
+  }, "En \uD83C\uDDEC\uD83C\uDDE7")), /*#__PURE__*/React.createElement("span", null, "Copyright 2024 Hanvesting All Rights Reserved"));
+}
+class MyAppA extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(NavbarA, null), /*#__PURE__*/React.createElement(HomeA, null), /*#__PURE__*/React.createElement(AboutMeA, null), /*#__PURE__*/React.createElement(ContactA, null), /*#__PURE__*/React.createElement(SkillsA, null), /*#__PURE__*/React.createElement(ProjectsA, null), /*#__PURE__*/React.createElement(FooterA, null));
+  }
+}
+ReactDOM.render( /*#__PURE__*/React.createElement(MyAppA, null), document.querySelector("#root"));
+var select = document.querySelector("select");
+document.querySelector("#show").style.direction = "rtl";
+document.querySelector(".me").style.direction = "rtl";
+for(let i=0;i<3;i++){
+  document.querySelector(".me").childNodes[i].style.position = "relative";
+  document.querySelector(".me").childNodes[i].style.right = "10%";
+}
+select.onchange = () => {
+  if(select.value == "en"){
+    select.value = "ar"
+    window.open("index.html")
+  }
+}
+var varia = document.querySelector(":root");
+var darkM = document.querySelector(".modeD");
+var toggle = document.querySelector(".tog");
+var about = document.querySelector("#about_me");
+var contact = document.querySelector("#contact");
+var skills = document.querySelector("#skills");
+var projects = document.querySelector("#projects");
+var nav = document.querySelector(".navbar");
+var show = document.querySelector("#show");
+var foot = document.querySelector("footer");
+function b() {
+  if(toggle.classList[0] === "active" || toggle.classList[1] === "active"){
+    toggle.classList.replace("active","noActive");
+    about.style.backgroundImage = 'url("../images/AboutMeL.png")';
+    contact.style.backgroundImage = 'url("../images/conL.png")';
+    skills.style.backgroundImage = 'url("../images/AboutMeL.png")';
+    projects.style.backgroundImage = 'url("../images/conL.png")';
+    nav.style.background = "rgba(216, 217, 218,.7)";
+    show.style.background = "rgba(216, 217, 218,.7)";
+    varia.style.setProperty("--black","#FFF");
+    varia.style.setProperty("--white","#000");
+    varia.style.setProperty("--dark","#e8e9ea");
+    varia.style.setProperty("--light","#212529");
+  }
+  else if(toggle.classList[0] === "noActive" || toggle.classList[1] === "noActive"){
+    toggle.classList.replace("noActive","active");
+    about.style.backgroundImage = 'url("../images/AboutMeD.png")'
+    contact.style.backgroundImage = 'url("../images/conD.png")'
+    skills.style.backgroundImage = 'url("../images/AboutMeD.png")'
+    projects.style.backgroundImage = 'url("../images/conD.png")'
+    nav.style.background = "rgba(21, 25, 29,.7)";
+    show.style.background = "rgba(21, 25, 29,.7)";
+    varia.style.setProperty("--black","#000");
+    varia.style.setProperty("--white","#FFF");
+    varia.style.setProperty("--dark","#212529");
+    varia.style.setProperty("--light","#e8e9ea");
+  }
+}
+darkM.onclick = b
